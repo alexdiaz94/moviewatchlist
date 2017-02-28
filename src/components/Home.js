2ee4312acd4e2559de2a2ed42a6637aa881749d8
alex diaz <alexdiaz094@gmail.com>
2017-02-28 11:45:46 -0500

    tidy

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,14 +12,13 @@ class Home extends React.Component {
         <div style={{height:300,margin:20}}>
           <React_Boostrap_Carousel animation={true} className="carousel-fade">
             <div className="first">
-              <img src="http://www.joblo.com/posters/images/full/rogue_one_a_star_wars_story_ver5_xxlg.jpg" height="400" width="60%"/>
-            </div>
-            <div className="second">
-              <img src="http://www.joblo.com/posters/images/full/john-wick-fposter-gallery.jpg" height="400" width="60%"/>
-            </div>
-            <div className="third">
-            <img src="http://www.joblo.com/posters/images/full/the-last-jedi.jpg" height="400" width="60%"/>
-
+              <img src="http://www.joblo.com/posters/images/full/rogue_one_a_star_wars_story_ver5_xxlg.jpg" height="400" width="50%"/>
+                </div>
+                <div className="second">
+                <img src="http://www.joblo.com/posters/images/full/john-wick-fposter-gallery.jpg" height="400" width="50%"/>
+               </div>
+              <div className="third">
+             <img src="http://www.joblo.com/posters/images/full/the-last-jedi.jpg" height="400" width="50%"/>
             </div>
           </React_Boostrap_Carousel>
         </div>
